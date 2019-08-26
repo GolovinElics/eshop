8b967c58d64fb51c3753a1d4f05967147d5f890d
Golovin <golovin@qq.com>
2019-08-26 21:16:30 +0800

    Revert "Version 1.0.0"
    This reverts commit f07acce5

--- src/main/java/com/qingshixun/project/eshop/EshopWebApplication.java
+++ src/main/java/com/qingshixun/project/eshop/EshopWebApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EshopWebApplication {
 
+public class EshopWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(EshopWebApplication.class, args);
     }
